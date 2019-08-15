270f89cf7ddceb2d364d1ab9c40a40824c906d8f
Nick Bauters <nick.bauters@multimedi.be>
2019-08-15 12:51:13 +0200

    created barn class: Will hold the animals and feeders

---
+++ src/main/java/be/weloveanimals/animalFarm/barn/Barn.java
@@ -0,0 +1,46 @@
+package be.weloveanimals.animalFarm.barn;
+
+import java.util.ArrayList;
+
+public class Barn {
+    private int id;
+    private String name;
+    private Feeder feeder;
+    private List<Animal> animals = new ArrayList<Animal>();
+
+    public Barn() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Feeder getFeeder() {
+        return feeder;
+    }
+
+    public void setFeeder(Feeder feeder) {
+        this.feeder = feeder;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(List<Animal> animals) {
+        this.animals = animals;
+    }
+}
